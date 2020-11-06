6a3ab56a4bb06ac5aa3b2500cf33a12cce4ac46a
praptimane26 <praptimane26@github.com>
2020-11-06 15:39:34 +1300

    files copied from todds code

--- Assets/Scripts/Models/Player.cs
+++ Assets/Scripts/Models/Player.cs
@@ -10,13 +10,15 @@ public class Player
     private int health;
     private int wealth;
     private string password;
+
     // what about inventory?
 
-        public int PlayerID { get; set; }
+    public int PlayerID { get; set; }
     public string Name { get => name; set => name = value; }
     public int LocationId { get => location; set => location = value; }
     //public int LocationId { get => location; set => location = value; }
     public int Health { get => health; set => health = value; }
     public int Wealth { get => wealth; set => wealth = value; }
     public string Password { get => password; set => password = value; }
+    public int HighScore { get; set; }
 }
---
+++ Assets/Scripts/Models/TestJsnDropNetworkService.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TestJsnDropNetworkService : MonoBehaviour
+{
+    public void jsnReceiverDel(JsnReceiver pReceived)
+    {
+        Debug.Log(pReceived.JsnMsg + " ..." + pReceived.Msg);
+        // To do: parse and produce an appropriate response
+    }
+
+    public void jsnListReceiverDel(List<Player> pReceivedList)
+    {
+        Debug.Log("Received items " + pReceivedList.Count());
+        foreach (Player lcReceived in pReceivedList)
+        {
+            Debug.Log("Received {" + lcReceived.PlayerID + "," + lcReceived.Password + "," + lcReceived.HighScore.ToString()+"}");
+        }// for
+
+        // To do: produce an appropriate response
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Test jsn drop
+        JSONDropService jsDrop = new JSONDropService { Token = "6f26d3ba-60ae-484b-ac42-613fcf21fa19" };
+        
+        // Create table person
+        jsDrop.Create<Player, JsnReceiver>(new Player
+        {
+            PlayerID = 0,
+            HighScore = 0,
+            Password = "***************************"
+        }, jsnReceiverDel);
+        /*
+        // Store people records
+        jsDrop.Store<tblPerson,JsnReceiver> (new List<tblPerson>
+        {
+            new tblPerson{PersonID = "Jack",HighScore = 100,Password ="test"},
+            new tblPerson{PersonID = "Jonny",HighScore = 200,Password ="test1"},
+            new tblPerson{ PersonID = "Jill",HighScore = 300,Password ="test2"}
+         }, jsnReceiverDel);
+        
+        // Retreive all people records
+        jsDrop.All<tblPerson, JsnReceiver>(jsnListReceiverDel, jsnReceiverDel);
+        
+        jsDrop.Select<tblPerson,JsnReceiver>("HighScore > 200",jsnListReceiverDel, jsnReceiverDel);
+        
+        jsDrop.Delete<tblPerson, JsnReceiver>("PersonID = 'Jonny'", jsnReceiverDel);
+        */
+        jsDrop.Drop<Player, JsnReceiver>(jsnReceiverDel);
+        
+        #endregion
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
